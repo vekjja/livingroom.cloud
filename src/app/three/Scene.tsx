84964fa8076757982d3ea04fbfe272390b4bb8ad
seemywings <kevin.jayne@icloud.com>
2025-02-11 12:33:44 -0500

    adding three.js

--- src/app/three/Scene.tsx
+++ src/app/three/Scene.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { alpha, Box } from "@mui/material";
+import { Box } from "@mui/material";
 
 // Define Prop Types
 interface ThreeSceneProps {
