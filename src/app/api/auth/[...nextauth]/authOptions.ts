73d2494670e77a6a70a8d73c0f19adfe27d0364e
seemywings <kevin.jayne@icloud.com>
2025-02-11 00:51:46 -0500

    rm email login

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,7 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import DiscordProvider from "next-auth/providers/discord";
-import EmailProvider from "next-auth/providers/email";
+// import EmailProvider from "next-auth/providers/email";
 import { PrismaClient } from "@prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import LinkedInProvider, {
@@ -21,17 +21,17 @@ if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST || "",
-        port: Number(process.env.EMAIL_SERVER_PORT) || 0,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER || "",
-          pass: process.env.EMAIL_SERVER_PASSWORD || "",
-        },
-      },
-      from: process.env.EMAIL_FROM || "",
-    }),
+    // EmailProvider({
+    //   server: {
+    //     host: process.env.EMAIL_SERVER_HOST || "",
+    //     port: Number(process.env.EMAIL_SERVER_PORT) || 0,
+    //     auth: {
+    //       user: process.env.EMAIL_SERVER_USER || "",
+    //       pass: process.env.EMAIL_SERVER_PASSWORD || "",
+    //     },
+    //   },
+    //   from: process.env.EMAIL_FROM || "",
+    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID || "",
       clientSecret: process.env.GOOGLE_SECRET || "",
